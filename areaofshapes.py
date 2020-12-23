4d352b6b6669463b9a4de1f9ddd425fb8f82ec56
chiranjeevi08 <71633019+chiranjeevi08@users.noreply.github.com>
2020-12-23 11:32:29 +0530

    Update areaofshapes.py

--- areaofshapes.py
+++ areaofshapes.py
@@ -1,19 +1,21 @@
-
+from math import pi,tan
 
 def square(side):
 	area=side*4
 	return area
 def circle(radius):
-	area=3.14*radius**2
+	
+	area=pi*radius**2
 	return area
-def triangle(length,breath):
-	area=0.5*length*breath
+def polygon(sides,length):
+	
+	area=sides*(length**2)/(4**tan(pi/sides))
 	return area
 
 
 print("To Calculate Area OF Square Enter 1:")
 print("To Calculate Area OF circle Enter 2:")
-print("To Calculate Area OF Trinagle Enter 3:")
+print("To Calculate Area OF Polygon Enter 3:")
 opt=int(input("Enter Your Choice:"))
 if(opt==1):
 	side=int(input("Enter Length OF The Side In Centimeter:"))
@@ -25,10 +27,10 @@ elif(opt==2):
 	area=circle(radius)
 	print("Area Of Rectangle Is",area,"CM")
 elif(opt==3):
-	length=int(input("Enter Length Of The Triangle In Centimeter:"))
-	breath=int(input("Enter Breath Of The Triangle In Centimeter:"))
-	area=triangle(length,breath)
-	print("Arer Of Triangle Is",area)
+	side=int(input("Enter Number Of Sides:"))
+	length=int(input("Enter Length Of The Side In Centimeter:"))
+	area=polygon(side,length)
+	print("Arer Of polygon Is",area,"CM")
 else:
 	print("Enter A Valid Option")
 
